59e745a6371c65933c4ce5a495c7826edb362924
faik şahin bahçekapılı <62908221+zeftil@users.noreply.github.com>
2021-08-17 03:35:10 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace KampİntroAsıl_başlangıç
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety - tip güvenliği
+            //Do not repeat yourself - kendini tekrarlama
+            string kategoriEtiketi = "Kategoriler";
+            int ogrenciSayisi = 32000;
+            double faizOranı = 1.45;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.55;
+            double dolarBugun = 7.45;
+
+            if(dolarDun>dolarBugun)
+			{
+                Console.WriteLine("Azalış butonu");
+			}
+            else if (dolarDun<dolarBugun)
+			{
+                Console.WriteLine("Artış Butonu");
+			}
+
+
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+
+
+            Console.WriteLine(kategoriEtiketi);
+
+        }
+    }
+}
